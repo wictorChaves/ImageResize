8b0dfea16c2732d1c228036f097fde757e3600f8
Wictor Chaves <wictor@hotmail.com.br>
2021-09-11 11:56:55 -0300

    Alteração para Marilia.

--- ImageResize/Program.cs
+++ ImageResize/Program.cs
@@ -27,7 +27,6 @@ namespace ImageResize
             else
                 Environment.Exit(0);
 
-
             if (fbd.ShowDialog() == DialogResult.OK)
                 destino = fbd.SelectedPath;
             else
@@ -40,29 +39,54 @@ namespace ImageResize
                 var img = Image.FromFile(filePath);
                 var arrayPath = filePath.Split('\\');
                 var fileName = largura + "x" + altura + "_" + arrayPath[arrayPath.Length - 1];
-                ResizeImage_n_save(img, destino + "\\" + fileName, Convert.ToInt32(largura), Convert.ToInt32(altura));
+                ResizeImage(img, destino + "\\" + fileName, Convert.ToInt32(largura), Convert.ToInt32(altura));
             }
             Console.Write("Fim do processo");
             Console.ReadKey();
         }
 
-
-        public static void ResizeImage_n_save(Image image, string targetPath, int Width, int Height, bool enlarge = false, bool alignTop = false)
+        public static void ResizeImage(Image image, string targetPath, int Width, int Height, bool enlarge = false, bool alignTop = false)
         {
             decimal widthImage = image.Width;
             decimal heightImage = image.Height;
 
-            var maxWidth = Math.Min(Width, widthImage);
-            var maxHeight = Math.Min(Height, heightImage);
+            if (widthImage >= heightImage)
+                ResizeImageWidth(image, targetPath, Width, Height, widthImage, heightImage);
+            else
+                ResizeImageHeight(image, targetPath, Width, Height, widthImage, heightImage);
+        }
 
-            decimal adjustment = enlarge
-                ? Math.Max(maxWidth / widthImage, maxHeight / heightImage)
-                : Math.Min(maxWidth / widthImage, maxHeight / heightImage);
+        private static void ResizeImageHeight(Image image, string targetPath, int Width, int Height, decimal widthImage, decimal heightImage)
+        {
+            var percent = (100 * Height) / widthImage;
 
-            var newWidth = (int)Math.Round(widthImage * adjustment);
-            var newHeight = (int)Math.Round(heightImage * adjustment);
+            var newWidth = Width;
+            var newHeight = (int)Math.Round(heightImage * (percent / 100));
 
-            var positionY = alignTop ? 0 : Convert.ToInt32(Height * 0.5) - (newHeight / 2);
+            var positionY = Convert.ToInt32(Height * 0.65) - (newHeight / 2);
+            var positionX = 0;
+
+            var thumbnailImg = new Bitmap(Width, Height);
+            var thumbGraph = Graphics.FromImage(thumbnailImg);
+            thumbGraph.Clear(Color.White);
+            thumbGraph.CompositingQuality = CompositingQuality.HighQuality;
+            thumbGraph.SmoothingMode = SmoothingMode.HighQuality;
+            thumbGraph.InterpolationMode = InterpolationMode.HighQualityBicubic;
+
+            var imageRectangle = new Rectangle(positionX, positionY, newWidth, newHeight);
+
+            thumbGraph.DrawImage(image, imageRectangle);
+            thumbnailImg.Save(targetPath, image.RawFormat);
+        }
+
+        private static void ResizeImageWidth(Image image, string targetPath, int Width, int Height, decimal widthImage, decimal heightImage)
+        {
+            var percent = (100 * Height) / heightImage;
+
+            var newWidth = (int)Math.Round(widthImage * (percent / 100));
+            var newHeight = Height;
+
+            var positionY = 0;
             var positionX = Convert.ToInt32(Width * 0.5) - (newWidth / 2);
 
             var thumbnailImg = new Bitmap(Width, Height);
